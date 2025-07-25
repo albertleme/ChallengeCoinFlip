37b0748aa02aab2ff8b91e0567b44579b825e410
Albert  Leme <albert.leme@directy.com.br>
2025-07-25 10:16:46 -0300

    C# Foundamentals Challenge

--- Program.cs
+++ Program.cs
@@ -1,5 +1,56 @@
-﻿Random coin = new Random();
+﻿// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
 
-Console.Clear();
+string[] product = sku.Split('-');
 
-Console.WriteLine((coin.Next(2) == 0) ? "Heads" : "Tails");
+string type = product[0];
+string color = product[1];
+string size = product[2];
+
+switch (type)
+{
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat pants";
+        break;
+    default:
+        type = "Other";
+        break;
+}
+
+switch (color)
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+switch (size)
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One size fits all";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
